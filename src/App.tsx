181f85bcc4809b088196221fe5a2dc1ce254b2b3
Robert Pepka <robert_pepka@selinc.com>
2021-09-16 12:55:20 -0700

    Initial hello world app.

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Hello World!</h1>
+        <p>
+          This a hello world app.
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
