f8b97d799856012e1f3576d413b0a0e1068eeba7
Robert Pepka <robert_pepka@selinc.com>
2021-09-16 13:01:05 -0700

    Added todo item.

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,19 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { TodoItem } from './TodoItem';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Hello World!</h1>
-        <p>
-          This a hello world app.
-        </p>
+        <h1>Todos</h1>
+        <ul>
+          <TodoItem />
+          <TodoItem />
+          <TodoItem />
+          <TodoItem />
+        </ul>
       </header>
     </div>
   );
---
+++ src/TodoItem.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export function TodoItem() {
+  return (
+    <li>
+      <input type="text" />
+      <input type="checkbox" />Done
+    </li>
+  );
+}
